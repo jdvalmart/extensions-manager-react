d36ec6ff7ba5e3773002e1325fba6e0716cf6947
jdvalmart <juanvalencia9411@outlook.com>
2025-10-16 01:44:51 -0500

    se trabaja sobre los estilos en tailwind y la logica del botón para cambiar de color  del header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-
+import {Headers} from './components/Headers'
 
 function App() {
   
+  
 
   return (
-    <>
-      <h1>Hola mundo</h1>
-    </>
+  <main className='light-gradient dark:dark-gradient min-h-screen flex flex-col items-center'>
+    <section className='w-[343px] my-6'>
+       <Headers/>
+    </section>
+   
+  </main>
+      
+    
   )
 }
 
---
+++ src/components/Headers.jsx
@@ -0,0 +1,25 @@
+import logo from '../assets/images/logo.svg'
+import logodark from '../assets/images/logo-dark.svg'
+import iconmoon from '../assets/images/icon-moon.svg'
+import iconsun from '../assets/images/icon-sun.svg'
+import { useState } from 'react'
+
+
+export const Headers = () => {
+
+  const [isDark, setIsDark] = useState(false)
+
+  const handleClick = () => {
+    const isDarkChanged = document.documentElement.classList.toggle('dark')
+    setIsDark(isDarkChanged)
+  }
+  return (
+    <div className=' bg-Neutral-0 dark:bg-Neutral-800 flex justify-between h-[66px] px-3 py-2 rounded-[10px] items-center'>
+        <img src={ isDark ? logodark : logo} alt="logo" />
+       <button onClick={handleClick} className='bg-neutral-100 size-[50px] grid place-content-center rounded-lg cursor-pointer hover:bg-Neutral-200'>
+        <img src={iconmoon} alt="" />
+       </button>
+
+    </div>
+  )
+}
