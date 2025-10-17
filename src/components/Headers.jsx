9701f371d53b2282c8ca0f4d7f55483169f00ff9
jdvalmart <juanvalencia9411@outlook.com>
2025-10-17 01:36:24 -0500

    manejo de localStorage para mantener la pagina en dark o light al momento de refrescar

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -7,16 +7,23 @@ import { useState } from 'react'
 
 export const Headers = () => {
 
-  const [isDark, setIsDark] = useState(false)
+  const savedTheme = localStorage.getItem('dark')
+
+  const [isDark, setIsDark] = useState(JSON.parse(savedTheme))
+
+  if (JSON.parse(savedTheme)) {
+    document.documentElement.classList.add('dark')
+  }
 
   const handleClick = () => {
     const isDarkChanged = document.documentElement.classList.toggle('dark')
     setIsDark(isDarkChanged)
+    localStorage.setItem('dark', isDarkChanged )
   }
   return (
     <div className=' bg-Neutral-0 dark:bg-Neutral-800 flex justify-between h-[66px] px-3 py-2 rounded-[10px] items-center mb-6'>
         <img src={ isDark ? logodark : logo} alt="logo" />
-       <button onClick={handleClick} className='bg-neutral-100 size-[50px] grid place-content-center rounded-lg cursor-pointer hover:bg-Neutral-200 dark:bg-Neutral-00 dark:hover:bg-Neutral-600'>
+       <button onClick={handleClick} className='bg-neutral-100 size-[50px] grid place-content-center rounded-lg cursor-pointer hover:bg-Neutral-200 dark:bg-Neutral-600 dark:hover:bg-Neutral-700'>
         <img src={isDark ? iconsun  : iconmoon} alt="" />
        </button>
 
